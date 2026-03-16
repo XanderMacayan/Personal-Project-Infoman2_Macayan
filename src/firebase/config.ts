5bcb2ba839e4ebb72bb4bbd1bdf22b02c55e9ba0
Xander G. Macayan <xander.macayan@neu.edu.ph>
2026-03-16 04:12:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6642348290-cdb21",
+  "appId": "1:642590016604:web:981294f3fc353f297c5299",
+  "apiKey": "AIzaSyCWUUjDiTeULZHhTJ3DMg0sZbSE3fW6KzY",
+  "authDomain": "studio-6642348290-cdb21.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "642590016604"
+};
